7dce9c6f6c1093fe830c34003136db18b2428870
Lola Maïa Lou Bardel <lola.bardel@epfl.ch>
2024-04-18 14:56:25 +0200

    Organize sankey architecture (add js & data)

---
+++ website/js/sankey.js
@@ -0,0 +1,75 @@
+// dimension and margin of graph
+var margin = {top:10, right:10, bottom:10, left:200},
+width = 900 - margin.left - margin.right,
+height = 700 - margin.top - margin.bottom;
+
+//format variable
+var formatNumber = d3.format(",.0f") // zero decimal places
+format = function(d) {return formatNumber(d);},
+color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// append the svg object to the body of the page
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+.append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// set the sankey diagram properties
+var sankey = d3.sankey()
+.nodeWidth(20)
+.nodePadding(10)
+.size([width, height])
+
+var path = sankey.links();
+
+// load the data
+d3.json("../data_world/sankey_2020_continent_test.json").then(function(sankeydata){
+graph = sankey(sankeydata);
+
+// add the links
+var link = svg.append("g").selectAll(".link")
+.data(graph.links)
+.enter().append("path")
+    .attr("class", "link")
+    .attr("d", d3.sankeyLinkHorizontal())
+    .attr("stroke-width", function(d) {return d.width});
+
+// add the link titles that appear when hovering on the links
+link.append("title")
+.text(function(d) {
+    return "from " + d.source.name + " to " + d.target.name + "\n" + format(d.value);
+});
+
+// add in the nodes
+var node = svg.append("g").selectAll(".node")
+.data(graph.nodes)
+.enter().append("g")
+.attr("class", "node");
+
+// add the rectangles for the nodes 
+node.append("rect")
+.attr("x", function(d) {return d.x0;})
+.attr("y", function(d) {return d.y0;})
+.attr("height", function(d) {return d.y1 - d.y0; })
+.attr("width", sankey.nodeWidth())
+.style("fill", function(d) {
+                    return d.color = color(d.name.replace(/ .*/, "")); })
+.style("stroke", function(d) {
+                    return d3.rgb(d.color).darker(2); })
+.append("title")
+    .text(function(d) {
+                return d.name + "\n" + format(d.value); })
+
+// add the titles for the nodes
+node.append("text")
+.attr("x", function(d) {return d.x0-6;})
+.attr("y", function(d) {return (d.y1 + d.y0)/ 2;})
+.attr("dy", "0.35em")
+.attr("text-anchor", "end")
+.text(function(d) {return d.name;})
+.filter(function(d) {return d.x0 < width/2;})
+.attr("x", function(d) {return dx1 + 6;})
+.attr("text-anchor", "start")
+});
