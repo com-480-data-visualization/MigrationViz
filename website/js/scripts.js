2ac9dd4ae3dde89b10cce22e0d67621a37f92bc8
Rayjine <65818863+Rayjine@users.noreply.github.com>
2024-05-24 11:10:44 +0200

    Update scripts.js

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -5,21 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
     // Attachez un écouteur d'événements à tous vos liens de navigation pour le défilement fluide
     document.querySelectorAll('a').forEach(function(anchor) {
         anchor.addEventListener('click', function(e) {
-            e.preventDefault(); // Empêche le comportement par défaut du lien
-            // Utilise l'attribut href du lien pour trouver l'élément cible
             var targetID = this.getAttribute('href');
-            var targetSection = document.querySelector(targetID);
-            // Si la section cible existe, faites défiler vers elle
-            if (targetSection) {
-                targetSection.scrollIntoView({
-                    behavior: 'smooth'
-                });
+            // Appliquer le comportement seulement pour les liens internes (ceux avec un #)
+            if (targetID.startsWith('#')) {
+                e.preventDefault(); // Empêche le comportement par défaut du lien
+                var targetSection = document.querySelector(targetID);
+                // Si la section cible existe, faites défiler vers elle
+                if (targetSection) {
+                    targetSection.scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
             }
         });
     });
 
     updateMenu();
 });
+
 
 function updateMenu() {
     var sections = document.querySelectorAll('main .slide');
